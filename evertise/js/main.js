49b01fe1cae5305c4096d3d10bd78f2e413e8ee3
marmaliser <marmaliser31@gmail.com>
2019-06-16 21:01:29 +0100

    Initial commit all

--- evertise/js/main.js
+++ evertise/js/main.js
@@ -697,47 +697,6 @@
         })
     }
     
-    
-    // MAILCHIMP
-    if ($(".mailchimp").length > 0)
-    {
-        $(".mailchimp").ajaxChimp({
-            callback: mailchimpCallback,
-            url: "http://droitlab.us15.list-manage.com/subscribe/post?u=0fa954b1e090d4269d21abef5&id=a80b5aedb0" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".  
-        });
-    }
-    if ($(".mailchimp_two").length > 0)
-    {
-        $(".mailchimp_two").ajaxChimp({
-            callback: mailchimpCallback,
-            url: "https://droitthemes.us19.list-manage.com/subscribe/post?u=5d334217e146b083fe74171bf&amp;id=0e45662e8c" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".  
-        });
-    }
-    $(".memail").on("focus", function ()
-    {
-        $(".mchimp-errmessage").fadeOut();
-        $(".mchimp-sucmessage").fadeOut();
-    });
-    $(".memail").on("keydown", function ()
-    {
-        $(".mchimp-errmessage").fadeOut();
-        $(".mchimp-sucmessage").fadeOut();
-    });
-    $(".memail").on("click", function ()
-    {
-        $(".memail").val("");
-    });
-    function mailchimpCallback(resp)
-    {
-        if (resp.result === "success") {
-            $(".mchimp-errmessage").html(resp.msg).fadeIn(1000);
-            $(".mchimp-sucmessage").fadeOut(500);
-        } else if (resp.result === "error") {
-            $(".mchimp-errmessage").html(resp.msg).fadeIn(1000);
-        }
-    }
-    
-    
     /* ===== Parallax Effect===== */
 	function parallaxEffect() {
         if($('.parallax-effect').length){
